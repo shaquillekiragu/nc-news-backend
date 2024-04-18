516ac72d7bb4258f1c7692cc41c67bd4571efbed
Shaquille Kiragu <skiragu77@gmail.com>
2024-04-19 00:44:26 +0100

    10-get-users completed

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,1 +1,9 @@
-const {} = require("../models/users.model");
+const fetchUsers = require("../models/users.model");
+
+function getUsers(request, response, next) {
+  fetchUsers().then((users) => {
+    response.status(200).send({ users });
+  });
+}
+
+module.exports = getUsers;
