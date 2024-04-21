831524405b65123f585201876e9f8350299415db
Shaquille Kiragu <skiragu77@gmail.com>
2024-04-21 19:20:30 +0100

    Hosting database 3

--- db/connection.js
+++ db/connection.js
@@ -6,15 +6,15 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE or DATABASE_URL not set");
-}
-
 const config = {};
 
 if (ENV === "production") {
   config.connectionString = process.env.DATABASE_URL;
   config.max = 2;
+}
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
 module.exports = new Pool();
