3a91fb74c06db602f98acc46e761fb592b05bea0
Shaquille Kiragu <skiragu77@gmail.com>
2024-04-21 18:39:19 +0100

    Attempt to resolve merge conflict on main 2

--- db/connection.js
+++ db/connection.js
@@ -1,20 +1,12 @@
 const { Pool } = require("pg");
-
 const ENV = process.env.NODE_ENV || "development";
-
-const config = {};
 
 require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (ENV === "production") {
-  config.connectionString = process.env.DATABASE_URL;
-  config.max = 2;
-}
-
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error("PGDATABASE or DATABASE_URL not set");
+if (!process.env.PGDATABASE) {
+  throw new Error("PGDATABASE not set");
 }
 
 module.exports = new Pool();
